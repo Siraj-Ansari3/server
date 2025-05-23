c3b72a00486bd624e917af222913bca0b0c95fd3
SirajAnsari3 <siraj2022ansari@gmail.com>
2025-05-23 20:39:22 +0500

    commit

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ server.use(express.json());
 server.use(cookieParser());
 server.use(cors({
     origin: 'http://localhost:5173', // Your frontend URL
+    origin: 'https://lims.netlify.app',
     credentials: true
 }));
 
